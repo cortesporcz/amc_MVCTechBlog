1129c6f50c5e8d2c0c2499863583f91f80d3a6a7
Angelina Cortes <cortesporcz@gmail.com>
2024-03-07 16:32:02 -0600

    adding wireframe for homeroutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,13 @@
+// Imports
+// Serialize data so the template can read it
+// Pass serialized data and session flag into template
+// Route set up to find single blog post and render blogPost page
+// Join user data and comment data with blog post data
+// route to allow logged in user access to the dashboard page
+// Use withAuth middleware to prevent access to route
+ // Find the logged in user based on the session ID
+// Join user blog post and comment data with user data
+// NEW POST PAGE: Renders 'create.handlebars'; redirects to /login if not logged in
+// Route set up to be able to edit an existing blog post
+// Join user data and comment data with blog post data
+// If the user is already logged in, redirect the request to another route
