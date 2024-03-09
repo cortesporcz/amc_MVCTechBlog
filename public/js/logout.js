549fa2db991ba3af44a65ca7819447376f838830
Angelina Cortes <cortesporcz@gmail.com>
2024-03-09 17:29:11 -0600

    adding setup/comments to logout.js file

---
+++ public/js/logout.js
@@ -0,0 +1,4 @@
+// Function created that allows users to delete blog posts on dashboard page and then redirect them to an updated dashboard
+// Function created that allows for a user to edit blog posts on dashboard page by redirecting them to the /create/:id page
+// Iterates over all buttons on the page and allows for edit functionality
+// Iterates over all buttons on the page and allows for delete functionality
